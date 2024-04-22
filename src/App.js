e811829368d89db5c3b36a88473eb33532ccb311
Alina Li <alina.li@outlook.com>
2024-04-22 09:43:33 -0400

    replace logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 import Landing from './components/Landing';
@@ -9,21 +8,6 @@ function App() {
     <div className="App">
       <Header/>
       <Landing/>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
