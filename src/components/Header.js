798c2fafa63794d0a7e18ca9fc5381785cb550d7
Alina Li <alina.li@outlook.com>
2024-04-21 22:07:48 -0400

    add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className="fixed top-0 left-0 w-full h-40 bg-black opacity-50 flex items-end justify-end">
-            <div className="content-fill mb-5 mr-5 space-x-10">
-                <a className="text-white" href="tab_1">tab 1</a>
-                <a className="text-white" href="tab_2">tab 2</a>
-                <a className="text-white" href="tab_3">tab 3</a>
+        <div className="fixed top-0 left-0 w-full h-[5rem] bg-black opacity-50 flex items-end justify-end">
+            <div className="content-fill mb-5 mr-10 space-x-10">
+                <a className="text-white underline hover:underline-offset-4" href="tab_1">tab 1</a>
+                <a className="text-white underline hover:underline-offset-4" href="tab_2">tab 2</a>
+                <a className="text-white underline hover:underline-offset-4" href="tab_3">tab 3</a>
             </div>
         </div>
     );
