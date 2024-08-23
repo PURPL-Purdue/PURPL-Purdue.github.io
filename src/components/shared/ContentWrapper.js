17abb18b59757be19d63d41f84ae7e3d7b0cd930
Alina Li <alina.li@outlook.com>
2024-08-23 18:43:59 -0400

    Modify color-graded images (#11)
    * Add project wireframes
    
    * Make updates
    
    * Add discord link to landing
    
    * Change color-grading on images

--- src/components/shared/ContentWrapper.js
+++ src/components/shared/ContentWrapper.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ContentWrapper = ({ children }) => {
     return (
     <div className="flex flex-col w-full bg-dusk" id="contentWrapper">
-        <div className="flex flex-col mx-8 md:mx-12 lg:mx-auto max-w-[1000px] pt-8 lg:pt-8" id="paddedContentWrapper">
+        <div className="flex flex-col mx-8 md:mx-12 lg:mx-auto max-w-[1100px] pt-8 lg:pt-8" id="paddedContentWrapper">
             {children}
         </div>
     </div>
