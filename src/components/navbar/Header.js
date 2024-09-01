4dac84e4b1ce34268b8b532ef951f698c609a4cb
Alina Li <alina.li@outlook.com>
2024-09-01 17:15:21 -0400

    SEO improvements, update sponsors and calendar (#14)
    * Add project wireframes
    
    * Make updates
    
    * Add discord link to landing
    
    * Change color-grading on images
    
    * Add gradient line; upload headshots
    
    * Replace boilermaker gold with old gold color
    
    * Add gtags, fix some console errors
    
    * Update calendar; add cfturbo sponsor logo

--- src/components/navbar/Header.js
+++ src/components/navbar/Header.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import Dropdown from './Dropdown';
+import { FaBars, FaXmark } from 'react-icons/fa6';
 
 const Header = () => {
         const navLinks = [
@@ -90,113 +91,108 @@ const Header = () => {
 
         const [mobileActiveDropdowns, setMobileActiveDropdowns] = useState(Array(navLinks.length).fill(false));
 
-        const toggleMobileActiveDropdown = (id) => { 
-                console.log(mobileActiveDropdowns);  
-                console.log(mobileActiveDropdowns[id]); 
+        const toggleMobileActiveDropdown = (id) => {
+                console.log(mobileActiveDropdowns);
+                console.log(mobileActiveDropdowns[id]);
                 const updatedDropdowns = [...mobileActiveDropdowns];
                 updatedDropdowns[id] = !updatedDropdowns[id];
                 setMobileActiveDropdowns(updatedDropdowns);
         };
 
         return (
-        <div className="">
-                <div id="hamburger-menu" className="lg:invisible bg-dusk flex flex-row">
-                        <NavLink to="" className="mr-auto ml-8 mt-5"><img src="/images/logo/PURPL_white_logo.png" className="h-8 ">
-                        </img></NavLink>
-                        <button class="navbar-burger flex items-center text-white hover:text-stardust p-6" onClick={(e) => {
-                                setMobileMenuOpen(true);
-                        }}>
-                                <svg class="block h-6 w-6 fill-current" viewBox="0 0 20 20">
-                                        <title>Mobile menu</title>
-                                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
-                                </svg>
-                        </button>                
-                </div>
-                <div class={`navbar-menu relative z-50 lg:hidden ${mobileMenuOpen ? "visible" : "hidden"}`}>
-		<div class="navbar-backdrop fixed inset-0 bg-dusk opacity-25"></div>
-                        <nav class="fixed top-0 left-0 bottom-0 flex flex-col w-2/3 max-w-sm py-6 px-6 bg-dusk border-r border-white overflow-y-auto">
-                                <div class="flex items-center mb-8">
-                                        <a class="mr-auto text-3xl font-bold leading-none" href="#">
-                                                <svg class="h-12" alt="logo" viewBox="0 0 10240 10240">
-                                        </svg>
-                                        </a>
-                                        <button id="navbar-close" onClick={(e) => {
-                                                setMobileMenuOpen(false);
-                                        }}>
-                                                <svg class="h-6 w-6 text-white cursor-pointer hover:text-stardust" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-                                                </svg>
-                                        </button>
-                                </div>
-                                <div>
-                                        <ul>{
-                                        navLinks.map((navLink) => {
-                                                return (<li key={navLink.id} class="mb-1 text-left">
-                                                        {
-                                                                (navLink.children.length > 0) ?
-                                                                <p class="font-display block p-3 text-sm text-white hover:text-stardust rounded hover:cursor-pointer" onClick={(e) => { toggleMobileActiveDropdown(navLink.id)}}>{navLink.name} <span className="text-white">{(mobileActiveDropdowns[navLink.id] ? " v" : " >")}</span></p> :
-                                                                <NavLink to={navLink.link} className="font-display block p-3 text-sm text-white hover:text-stardust" >{navLink.name}</NavLink>
-
-                                                        }
-                                                        <ul className={`lg:hidden ${mobileActiveDropdowns[navLink.id] ? "visible" : "hidden"}`}>
-                                                              {
-                                                                (navLink.children).map((navChild) => {
-                                                                        return (<NavLink                        
-                                                                                to={navChild.link}
-                                                                                className="font-display block p-2 ml-4 text-sm text-white hover:text-stardust"
-                                                                        >{navChild.name}</NavLink>)
-                                                                })}
-                                                        </ul>
-
-                                                        </li>
-                                                )})
-                                        }</ul>
-                                </div>
-                        </nav>
-                </div>
-                <div id="standard-navbar" className={"invisible lg:visible fixed top-0 w-full h-28 z-10  px-16 bg-dusk "}
-                >
-                        <div className="mx-auto inset-x-0 w-full flex items-end justify-end fixed top-0 h-28 px-8 max-w-[1300px]">
-                        <div className="fixed top-28 left-0 w-full h-2 bg-gradient-to-b from-black to-transparent">
-
+                <div className="">
+                        <div id="hamburger-menu" className="lg:invisible bg-dusk flex flex-row">
+                                <NavLink to="" className="mr-auto ml-8 mt-5"><img src="/images/logo/PURPL_white_logo.png" className="h-8 ">
+                                </img></NavLink>
+                                <button className="flex items-center text-white hover:text-stardust p-6" onClick={(e) => {
+                                        setMobileMenuOpen(true);
+                                }}>
+                                        <FaBars className="h-6 w-6 text-white hover:text-stardust" />
+                                </button>
                         </div>
-                        <NavLink className="h-12 mr-auto mb-6" to=""><img className="h-12 mr-auto mb-3" src="/images/logo/PURPL_white_logo.png" alt="PURPL logo"></img></NavLink>
+                        <div className={`relative z-50 lg:hidden ${mobileMenuOpen ? "visible" : "hidden"}`}>
+                                <div className="fixed inset-0 bg-dusk opacity-25"></div>
+                                <nav className="fixed top-0 left-0 bottom-0 flex flex-col w-2/3 max-w-sm py-6 px-6 bg-dusk border-r border-white overflow-y-auto">
+                                        <div className="flex items-center mb-8">
+                                                <a className="mr-auto text-3xl font-bold leading-none" href="#">
+                                                        <svg className="h-12" alt="logo" viewBox="0 0 10240 10240">
+                                                        </svg>
+                                                </a>
+                                                <button id="navbar-close" onClick={(e) => {
+                                                        setMobileMenuOpen(false);
+                                                }}>
+                                                        <FaXmark className="h-6 w-6 text-white hover:text-stardust" />
+                                                </button>
+                                        </div>
+                                        <div>
+                                                <ul>{
+                                                        navLinks.map((navLink) => {
+                                                                return (<li key={navLink.id} className="mb-1 text-left">
+                                                                        {
+                                                                                (navLink.children.length > 0) ?
+                                                                                        <p className="font-display block p-3 text-sm text-white hover:text-stardust rounded hover:cursor-pointer" onClick={(e) => { toggleMobileActiveDropdown(navLink.id) }}>{navLink.name} <span className="text-white">{(mobileActiveDropdowns[navLink.id] ? " v" : " >")}</span></p> :
+                                                                                        <NavLink to={navLink.link} className="font-display block p-3 text-sm text-white hover:text-stardust" >{navLink.name}</NavLink>
 
-                        <div className="content-fill mb-6 space-x-4">
-                                {navLinks.map((navLink) => {
-                                        return (
-                                                <div className="inline-block"
-                                                        key={navLink.id} 
-                                                        onHover={() => setActiveDropdown(navLink.name)}
-                                                        onMouseEnter={() => setActiveDropdown(navLink.name)}
-                                                        onMouseLeave={() => setActiveDropdown(null)}
-                                                >
-                                                        {(navLink.link === "#") ?
-                                                                <div className={"font-display text-xl hover:text-stardust hover:border-b-2 hover:border-stardust w-32 inline-block "
-                                                                        + (activeDropdown === navLink.name ? "text-stardust border-b-2 border-stardust" : "text-white")
-                                                                }>{navLink.name}</div>
-                                                                :
-                                                                <NavLink
-                                                                        to={navLink.link}
-                                                                        className={({ isActive, isPending }) =>
-                                                                                ("font-display text-xl hover:text-stardust w-32 inline-block " +
-                                                                                        ((isActive || (activeDropdown === navLink.name))
-                                                                                                ? "text-stardust border-b-2 border-stardust"
-                                                                                                : isPending
-                                                                                                        ? "text-stardust"
-                                                                                                        : "text-white"))
                                                                         }
-                                                                >{navLink.name}</NavLink>
+                                                                        <ul className={`lg:hidden ${mobileActiveDropdowns[navLink.id] ? "visible" : "hidden"}`}>
+                                                                                {
+                                                                                        (navLink.children).map((navChild) => {
+                                                                                                return (<NavLink
+                                                                                                        to={navChild.link}
+                                                                                                        className="font-display block p-2 ml-4 text-sm text-white hover:text-stardust"
+                                                                                                >{navChild.name}</NavLink>)
+                                                                                        })}
+                                                                        </ul>
 
-                                                        }
-                                                        {(navLink.children.length > 0 && <Dropdown menuItems={navLink.children} isActiveProp={activeDropdown === navLink.name} />)}
-                                                </div>
-                                        );
-                                })}
+                                                                </li>
+                                                                )
+                                                        })
+                                                }</ul>
+                                        </div>
+                                </nav>
                         </div>
+                        <div id="standard-navbar" className={"invisible lg:visible fixed top-0 w-full h-28 z-10  px-16 bg-dusk "}
+                        >
+                                <div className="mx-auto inset-x-0 w-full flex items-end justify-end fixed top-0 h-28 px-8 max-w-[1300px]">
+                                        <div className="fixed top-28 left-0 w-full h-2 bg-gradient-to-b from-black to-transparent">
+
+                                        </div>
+                                        <NavLink className="h-12 mr-auto mb-6" to=""><img className="h-12 mr-auto mb-3" src="/images/logo/PURPL_white_logo.png" alt="PURPL logo"></img></NavLink>
+
+                                        <div className="content-fill mb-6 space-x-4">
+                                                {navLinks.map((navLink) => {
+                                                        return (
+                                                                <div className="inline-block"
+                                                                        key={navLink.id}
+                                                                        onMouseEnter={() => setActiveDropdown(navLink.name)}
+                                                                        onMouseLeave={() => setActiveDropdown(null)}
+                                                                >
+                                                                        {(navLink.link === "#") ?
+                                                                                <div className={"font-display text-xl hover:text-stardust hover:border-b-2 hover:border-stardust w-32 inline-block "
+                                                                                        + (activeDropdown === navLink.name ? "text-stardust border-b-2 border-stardust" : "text-white")
+                                                                                }>{navLink.name}</div>
+                                                                                :
+                                                                                <NavLink
+                                                                                        to={navLink.link}
+                                                                                        className={({ isActive, isPending }) =>
+                                                                                        ("font-display text-xl hover:text-stardust w-32 inline-block " +
+                                                                                                ((isActive || (activeDropdown === navLink.name))
+                                                                                                        ? "text-stardust border-b-2 border-stardust"
+                                                                                                        : isPending
+                                                                                                                ? "text-stardust"
+                                                                                                                : "text-white"))
+                                                                                        }
+                                                                                >{navLink.name}</NavLink>
+
+                                                                        }
+                                                                        {(navLink.children.length > 0 && <Dropdown menuItems={navLink.children} isActiveProp={activeDropdown === navLink.name} />)}
+                                                                </div>
+                                                        );
+                                                })}
+                                        </div>
+                                </div>
                         </div>
                 </div>
-        </div>
         );
 };
 
