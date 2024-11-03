585aa04cefe3f831114ce7db2f3718165a13ba0a
Alina Li <alina.li@outlook.com>
2024-11-03 14:00:23 -0500

    Add sponsors

--- src/components/team/Profile.js
+++ src/components/team/Profile.js
@@ -5,29 +5,31 @@ const Profile = (props) => {
     const { imagePath, name, title, email, linkedin } = props;
     return (
         <div className="w-36 lg:w-48 ">
-        <div className="flex flex-col items-center h-fit">
-            <div className="bg-gradient-to-b from-stardust to-old-gold w-full h-full">
-                <div className="m-1 bg-dusk">
-                    <img className={`w-36 lg:w-48 mx-auto`} src={imagePath} alt="Profile Image" />
+            <div className="flex flex-col items-center h-fit">
+                <div className="bg-gradient-to-b from-stardust to-old-gold w-full h-full">
+                    <div className="m-1 bg-dusk">
+                        <img className={`w-36 lg:w-48 mx-auto`} src={imagePath} alt="Profile Image" />
+                    </div>
+                </div>
+                <div className="flex flex-col items-center bg-dusk w-full ">
+                    <h2 className="font-display2-bold text-white text-md mb-1 mt-3">{name}</h2>
+                    <h3 className="font-display2 text-sm md:text-md text-white mb-1">{title}</h3>
+                    <div className="flex flex-row items-center gap-2 mb-2">
+                        {email ?
+                            <a href={`mailto:${email}`} target="_blank" rel="noopener noreferrer">
+                                <FaRegEnvelope className="email-icon fill-white hover:fill-stardust" />
+                            </a> :
+                            <span></span>
+                        }
+                        {linkedin ?
+                            <a href={`${linkedin}`} target="_blank" rel="noopener noreferrer">
+                                <FaLinkedinIn className="text-white hover:text-stardust" size={24} />
+                            </a> :
+                            <span></span>
+                        }
+                    </div>
                 </div>
             </div>
-            <div className="flex flex-col items-center bg-dusk w-full ">
-                <h2 className="font-display2-bold text-white text-md mb-1 mt-3">{name}</h2>
-                <h3 className="font-display2 text-sm md:text-md text-white mb-1">{title}</h3>
-                <div className="flex flex-row items-center gap-2 mb-2">
-                    {email ?
-                    <a href={`mailto:${email}`} target="_blank" rel="noopener noreferrer">
-                        <FaRegEnvelope className="email-icon fill-white hover:fill-stardust" />
-                    </a> :
-                    <span></span>}
-                    {linkedin ? 
-                        <a href= {`${linkedin}`} target="_blank" rel="noopener noreferrer">
-                        <FaLinkedinIn className="text-white hover:text-stardust" size={24} />
-                        </a> :
-                        <span></span>}
-                </div>
-            </div>
-        </div>
         </div>
     );
 };
