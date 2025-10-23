9c164caa3ef2937109648fad05b607f53ef90ced
seasew <alina.li@outlook.com>
2025-10-23 14:30:04 -0400

    Add Pulsejet

--- src/json/routing.js
+++ src/json/routing.js
@@ -35,6 +35,10 @@ export const navLinks = [
                 name: "torch training program",
                 link: "ttp",
             },
+            {
+                name: "pulsejet",
+                link: "pulsejet",
+            }
         ],
     },
     {
