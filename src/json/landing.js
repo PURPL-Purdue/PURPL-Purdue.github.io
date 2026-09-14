7579eecb1d03eaf69509bf0e46ad72e39cc9de3f
brettp13 <bpaden@purdue.edu>
2026-09-14 18:44:26 -0400

    Update calendar embed link in landing.js (#169)
    * Update calendar embed link in landing.js
    
    * Update calendar embed link in landing.js
    
    Put the normal (not embed) link, this fixes it

--- src/json/landing.js
+++ src/json/landing.js
@@ -16,7 +16,7 @@ export const landingData = {
         { number: "2024", text: "founded" },
     ],
     calendar: {
-        embed_link: "https://calendar.google.com/calendar/embed?src=purpl24%40purdue.edu&ctz=America%2FIndiana%2FIndianapolis",
+        embed_link: "https://calendar.google.com/calendar/embed?src=purpl.purdue%40gmail.com&ctz=America%2FNew_York",
     },
     carouselPhotos1: [
         teamPhotos.entire_club.fun_photo,
